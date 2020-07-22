a6ebe7770767975f147249842b8ae6e6e8ea04f0
Daniel Demerin <ddemerin.code@gmail.com>
2020-07-22 14:22:57 -0400

    Basic components in place

---
+++ src/components/Card.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Card = () => {
+  const [kittehs, setKittehs] = useState([{ authors: [] }])
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    const loadKittehs = async () => {
+      const resp = await axios.get('http://sdg-octodex.herokuapp.com')
+      setKittehs((prevKittehs) => {
+        prevKittehs = resp.data.data
+        return prevKittehs
+      })
+      setIsLoaded(true)
+    }
+    loadKittehs()
+  }, [])
+
+  if (isLoaded) {
+    return (
+      <>
+        <main>
+          <ul>
+            <section>
+              {kittehs.map((cat) => {
+                return (
+                  <div>
+                    <li class="kittehs">
+                      <img src={cat.image} alt={cat.alt} />
+                    </li>
+                    <p>
+                      {cat.number}: <span>{cat.name}</span>
+                      <section className="authors">
+                        {cat.authors.map((author) => {
+                          return (
+                            <a href={author.link}>
+                              <img
+                                src={author.image}
+                                className="icon"
+                                alt="author"
+                              />
+                            </a>
+                          )
+                        })}
+                      </section>
+                    </p>
+                  </div>
+                )
+              })}
+            </section>
+          </ul>
+        </main>
+      </>
+    )
+  } else {
+    return <div>Loading Kittehs...</div>
+  }
+}
+
+export default Card
